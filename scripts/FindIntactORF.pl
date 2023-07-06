b7c1aedff8ad14f73d89c295fae75562256bfef7
ammeix138 <ammeix138@hotmail.com>
2023-07-06 08:58:04 -0700

    Update FindIntactORF.pl
    Added comments

--- scripts/FindIntactORF.pl
+++ scripts/FindIntactORF.pl
@@ -1,3 +1,4 @@
+#Locating the intact open reading frames (ORFs) in each of the extracted L1 sequences by finding the longest alignment to ORF1 and ORF2 among all found ORFs using the Blastp file as input.
 while (<>) {
 @F=split/\s+/;
 print if $F[8] eq 1 and $F[9] eq 338 and $F[23] eq 1 and $F[24] eq 1275
