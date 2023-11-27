ca050f9d7c0656a78ea53742a6b866934c0a4820
leiyangly <leiyangly@gmail.com>
2023-11-27 14:29:29 -0800

    Update HaLoLIFe.sh so that it's compatible with 1000G_ONT

--- HaLoLIFe.sh
+++ HaLoLIFe.sh
@@ -5,20 +5,21 @@ gd=$3
 wd=$4
 pd=$5
 rg=$6
+gfn=$7
+rfn=$8
 
-
-#Extract the coordinate of the full length L1s
-perl ${pd}/scripts/ExtractL1.pl ${gd}/${ind}.${hap}.f1_assembly_v2_genbank_rm.bed > ${wd}/${ind}.${hap}.FL.bed
+#Extract the coordinate of the full length young L1s (L1HS, L1PA2 and L1PA3)
+perl ${pd}/scripts/ExtractL1.pl ${gd}/${ind}.${hap}.${rfn} > ${wd}/${ind}.${hap}.FL.bed
 
 #Extract the sequence of the full length L1s
 cat ${wd}/${ind}.${hap}.FL.bed |\
 perl -lane 'print if $F[5] eq "\+"' |\
-seqtk subseq ${gd}/${ind}.${hap}.f1_assembly_v2_genbank.fa.gz - |\
+seqtk subseq ${gd}/${ind}.${hap}.${gfn} - |\
 seqtk seq -U -l 0 - > ${wd}/${ind}.${hap}.FL.fa
 
 cat ${wd}/${ind}.${hap}.FL.bed |\
 perl -lane 'print if $F[5] eq "\-"' |\
-seqtk subseq ${gd}/${ind}.${hap}.f1_assembly_v2_genbank.fa.gz - |\
+seqtk subseq ${gd}/${ind}.${hap}.${gfn} - |\
 seqtk seq -U -r -l 0 - >> ${wd}/${ind}.${hap}.FL.fa
 
 
@@ -26,12 +27,12 @@ seqtk seq -U -r -l 0 - >> ${wd}/${ind}.${hap}.FL.fa
 #Extract the flanking 2kb regions of L1
 cat ${wd}/${ind}.${hap}.FL.bed |\
 perl -lane 'BEGIN{$"="\t"} $F[2]=$F[1]; $F[1]=$F[1]-2000; print "@F"' |\
-seqtk subseq ${gd}/${ind}.${hap}.f1_assembly_v2_genbank.fa.gz - |\
+seqtk subseq ${gd}/${ind}.${hap}.${gfn} - |\
 seqtk seq -U -l 0 - > ${wd}/${ind}.${hap}.FL-2kb.fa
 
 cat ${wd}/${ind}.${hap}.FL.bed |\
 perl -lane 'BEGIN{$"="\t"} $F[1]=$F[2]; $F[2]=$F[2]+2000; print "@F"' |\
-seqtk subseq ${gd}/${ind}.${hap}.f1_assembly_v2_genbank.fa.gz - |\
+seqtk subseq ${gd}/${ind}.${hap}.${gfn} - |\
 seqtk seq -U -l 0 - > ${wd}/${ind}.${hap}.FL+2kb.fa
 
 #Use minimap2 to map the flanking regions of L1s to the reference genome
@@ -65,18 +66,19 @@ perl ${pd}/scripts/FindIntactORF.pl ${wd}/${ind}.${hap}.FLAllORF.combine.blastp 
 #(2)only the minimap2 alignments that are longer than 200bp are taken. I tried various lengths, but setting this too long will make some of the regions unable to align. I think for the purpose of finding the rough liftover position, using short cutoffs makes sense. Setting this number to even ~1.4kb will not change the results for intact L1s of HG02622, but we don't know what will happen when we investigate more genomes
 #(3)only take the coordinates of minimap mapping when the plus and minus strand lifted over to the similar chromosomal locations.
 #(4)prefer to take the alignments on chromosomes instead of the decoys.
+#(5)written to be compatible with chromosome/scaffold names with underlines in them
 perl ${pd}/scripts/CombineTable.pl ${wd}/${ind}.${hap}.FL+2kb.${rg}.minimap.txt ${wd}/${ind}.${hap}.FL-2kb.${rg}.minimap.txt ${wd}/${ind}.${hap}.FLAllORF.intact.blastp ${wd}/${ind}.${hap}.FL.bed > ${wd}/${ind}.${hap}.${rg}.HaLoLIFe.output.txt
 
 
-#Retrieve the sequence of all the L1s with intact ORFs:
+#Retrieve the sequence of all the L1s with intact ORFs, this extraction step is compatible with chromosome/scaffold names with underlines in them
 cat ${wd}/${ind}.${hap}.${rg}.HaLoLIFe.output.txt |\
-perl -lane '@G=split/\_/,$F[0]; print "$G[0]\t$G[1]\t$G[2]" if $G[3] eq "\+" and $G[6] eq "intact"' |\
-seqtk subseq ${gd}/${ind}.${hap}.f1_assembly_v2_genbank.fa.gz - |\
+perl -lane '$"="\_"; @G=split/\_/,$F[0]; $len=scalar(@G)-7; $name="@G[0..$len]"; print "$name\t$G[-6]\t$G[-5]" if $G[-4] eq "\+" and $G[-1] eq "intact"' |\
+seqtk subseq ${gd}/${ind}.${hap}.${gfn} - |\
 seqtk seq -U -l 0 - > ${wd}/${ind}.${hap}.FLAllORF.intact.fa
 
 cat ${wd}/${ind}.${hap}.${rg}.HaLoLIFe.output.txt |\
-perl -lane '@G=split/\_/,$F[0]; print "$G[0]\t$G[1]\t$G[2]" if $G[3] eq "\-" and $G[6] eq "intact"' |\
-seqtk subseq ${gd}/${ind}.${hap}.f1_assembly_v2_genbank.fa.gz - |\
+perl -lane '$"="\_"; @G=split/\_/,$F[0]; $len=scalar(@G)-7; $name="@G[0..$len]"; print "$name\t$G[-6]\t$G[-5]" if $G[-4] eq "\-" and $G[-1] eq "intact"' |\
+seqtk subseq ${gd}/${ind}.${hap}.${gfn} - |\
 seqtk seq -U -r -l 0 - >> ${wd}/${ind}.${hap}.FLAllORF.intact.fa
 
 
