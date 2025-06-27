0596a49af779de22c71e5798620401a78ccee47d
leiyangly <leiyangly@gmail.com>
2025-06-27 13:49:39 -0700

    Remove legacy directory and update tests

--- legacy/ExtractL1.pl
+++
@@ -1,8 +0,0 @@
-#Working to extract the sequences from the full legnth L1s (sequences >= 5000bp) from the input .bed file.
-
-while (<>) {
-@F=split/\s+/;
-
-print if $F[3]=~m/^L1/ and $F[2]-$F[1] >= 5000
-
-}
